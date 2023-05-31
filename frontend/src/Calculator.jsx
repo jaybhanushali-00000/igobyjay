996c3626500920c59b18037adcda1c4d137044cd
jaybhanushali-00000 <131008728+jaybhanushali-00000@users.noreply.github.com>
2023-05-31 15:38:24 +0530

    Update Calculator.jsx

--- frontend/src/Calculator.jsx
+++ frontend/src/Calculator.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 
 export const Calculator=() => {
 
-let ENDPOINT ="https://igobyjay-1-jaybhanushali-00000.vercel.app/";
+let ENDPOINT ="https://igobyjay-1-omvapgz38-jaybhanushali-00000.vercel.app/";
 const[num1,setNum1] = useState();
 const[num2,setNum2] = useState();
 const[num3,setNum3] = useState();
