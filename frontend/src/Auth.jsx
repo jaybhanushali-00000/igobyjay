3896f2dc170cc3088e5d31afc8a673d98a9de502
Jay Rajesh Bhanushali <131008728+jaybhanushali-00000@users.noreply.github.com>
2023-06-09 19:23:38 +0530

    Update Auth.jsx

--- frontend/src/Auth.jsx
+++ frontend/src/Auth.jsx
@@ -5,7 +5,7 @@ import { Calculator } from "./Calculator";
 
 export const Auth=(props ) => {
 
-    let ENDPOINT ="https://igobyjay-1.vercel.app/";
+    let ENDPOINT ="https://igobyjay.hopto.org/";
     const [user_id,setUSR] = useState();
     const [password,setPass] = useState();
     const [b,setB] = useState(1);
