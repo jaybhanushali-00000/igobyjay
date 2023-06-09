11d43afa368194ff05c7c23276bf2b9c122fbc07
Jay Rajesh Bhanushali <131008728+jaybhanushali-00000@users.noreply.github.com>
2023-06-09 16:59:37 +0530

    Update Auth.jsx

--- frontend/src/Auth.jsx
+++ frontend/src/Auth.jsx
@@ -5,7 +5,7 @@ import { Calculator } from "./Calculator";
 
 export const Auth=(props ) => {
 
-    let ENDPOINT ="http://68.183.86.221/";
+    let ENDPOINT ="https://68.183.86.221/";
     const [user_id,setUSR] = useState();
     const [password,setPass] = useState();
     const [b,setB] = useState(1);
